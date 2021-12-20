e7dfdab4cc724e4f29610b807c847913302ef334
omaus <omaus@bio.uni-kl.de>
2021-12-20 20:53:22 +0100

    Add modules for color gradients

---
+++ src/FSharpColors/ColorGradient.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharpColors
+
+/// Functions for creating and working with color gradients.
+module ColorGradient =
+    
+    /// Functions for creating and working with one-dimensional color gradients.
+    module 1D =
+
+        /// Creates a gradient from a list of colors. The colors are equally spaced.
+        let create colorList =
+
+    // /// Functions for creating and working with two-dimensional color gradients.
+    // module 2D =
+
+    // /// Functions for creating and working with three-dimensional color gradients.
+    // module 3D =
